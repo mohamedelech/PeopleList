d286237a1c5b688d2e19649720554613bef60dca
mohamedelech <mohamed.elech@he-arc.ch>
2016-10-20 09:13:46 +0200

    ajout de la classe Person

---
+++ src/java/ch/hearc/ig/odi/peoplelist/business/Person.java
@@ -0,0 +1,66 @@
+package ch.hearc.ig.odi.peoplelist.business;
+
+import java.util.Date;
+
+public class Person {
+    
+    private Long id;
+    private String gender;
+    private String firstName;
+    private String lastName;
+    private Boolean married;
+    private Date birthDate;
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(Boolean married) {
+        this.married = married;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+}
